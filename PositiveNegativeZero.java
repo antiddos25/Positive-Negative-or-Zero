009b4ca1af41b60e82dbf0584997dedc36de0a0a
unknown <nguyenhungthinh2504@gmail.com>
2022-05-26 21:46:19 +0700

    first commit

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,15 @@
+public class PositiveNegativeZero {
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkNumber(-1);
+    }
+}
